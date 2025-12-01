6cbd8950f0f5b0d06c8fc250b25e8f18e2d747e0
cherkasovaa <alina.ch1202@gmail.com>
2025-12-01 15:18:57 +0300

    feat: OpenAPI spec in `doc` folder corresponds with assignment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
+import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +16,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const swaggerFilePath = join(__dirname, '..', 'doc', 'api.yaml');
+
+  try {
+    const apiSpecText = await readFile(swaggerFilePath, 'utf8');
+    const document = load(apiSpecText) as OpenAPIObject;
+
+    SwaggerModule.setup('doc', app, document);
+  } catch (error) {
+    console.error('Failed to load OpenAPI spec:', error.message);
+  }
 
   await app.listen(process.env.PORT || 4000);
 }
